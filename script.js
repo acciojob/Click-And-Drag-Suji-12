c456271bf1e1da95f86bcf77133fc44e7c18fda9
Suji-12 <23csec23sujithas@gmail.com>
2025-11-26 19:49:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,9 @@ let scrollLeft;
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
 
-slider.addEventListener('mouseleave', () => {
-  isDown = false;
-  slider.classList.remove('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;       
 });
 
 slider.addEventListener('mouseup', () => {
@@ -22,10 +18,16 @@ slider.addEventListener('mouseup', () => {
   slider.classList.remove('active');
 });
 
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
 slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-  e.preventDefault();
+  if (!isDown) return;       
+  e.preventDefault();     
+
   const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2;
+  const walk = (x - startX) * 2;  
   slider.scrollLeft = scrollLeft - walk;
 });
